da238de36d62389dcd1a7983ab228124e765822f
Marina1997Melad <meladmarina8@gmail.com>
2023-08-10 12:04:16 -0700

    dynamic allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocate memory
+ * @b: number of bytes
+ * Return: pointer string
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(b);
+	if (str == NULL)
+		exit(98);
+	return (str);
+}

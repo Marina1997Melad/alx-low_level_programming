bb686038dd9a3d2e98f635125685868b8c047788
Marina1997Melad <meladmarina8@gmail.com>
2023-08-10 13:30:21 -0700

    dynamic allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,26 +9,29 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len = 0, s2_len = 0, i;
+	unsigned int s1_len = 0, s2_len = 0, i, j;
 	char *s;
 
 	if (s1 == NULL)
 		s1 = " ";
 	if (s2 == NULL)
 		s2 = " ";
-	while (s1[s1_len] != '\0')
-		s1_len++;
-	while (s2[s2_len] != '\0')
-		s2_len++;
-	if (n >= s2_len)
-		n = s2_len;
+	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+		;
+	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+		;
 	s = malloc(sizeof(char) * n + s1_len + 1);
 	if (s == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < s1_len; i++)
+	}
+	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
-	for (i = 0; i < n; i++)
-		s[i + s1_len] = s2[i];
-	s[i + s1_len] = '\0';
+	for (j = 0; j < n; j++)
+	{
+		s[i] = s2[j];
+		i++;
+	}
+	s[i] = '\0';
 	return (s);
 }

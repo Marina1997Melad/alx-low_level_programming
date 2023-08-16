134bca9115b1becbee4ff76c4c0e4a5fcf8434cc
Marina1997Melad <meladmarina8@gmail.com>
2023-08-16 11:20:45 -0700

    func pointer

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNC_POINTERS_H
+#define FUNC_POINTERS_H
+
+#include <stddef.h>
+
+int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+void print_name(char *name, void (*f)(char *));
+
+#endif

002849a36983e3d439d290c0a7ebc91a67defcc3
Marina1997Melad <meladmarina8@gmail.com>
2023-08-31 15:26:58 -0700

    my commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
-	if (index > (sizeof(unsigned long int * 8 - 1))
-			return (-1);
-			mask <<= index;
-			*n = *n | mask;
-			return (1);
+	if (index >= sizeof(n) * 8)
+	{		return (-1);
+
+	}
+			return (n >> index & 1);
 }
